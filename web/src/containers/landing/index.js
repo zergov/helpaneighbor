97ad9f13a9fa5e082ebe9893b8dd313a9cc89e01
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-19 22:46:11 -0400

    add react app

---
+++ web/src/containers/landing/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function Landing() {
+  return (
+    <div className="container"style={{ height: "100vh" }}>
+      <div className="row h-100">
+        <div className="col-sm-12 my-auto">
+          <h1 className="text-center">Who are you?</h1>
+          <button type="button" className="btn btn-outline-primary btn-lg btn-block">Someone seeking for help</button>
+          <button type="button" className="btn btn-outline-primary btn-lg btn-block">Someone wanting to help</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
