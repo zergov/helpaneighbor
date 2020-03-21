b95abf356c09bf6f6c0b8c46a10184cddaeac293
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-21 15:31:36 -0400

    move locationsearchinput in its own component file

--- web/src/containers/Landing/WhereAreYou/index.js
+++ web/src/containers/Landing/WhereAreYou/index.js
@@ -3,73 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 import GoogleMapReact from 'google-map-react';
 
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
-
-class LocationSearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { address: '' };
-  }
-
-  handleChange = address => {
-    this.setState({ address });
-  };
-
-  handleSelect = async address => {
-    const results = await geocodeByAddress(address)
-    const latLng = await getLatLng(results[0])
-    if (this.props.onSelect) {
-      this.props.onSelect(latLng)
-    }
-  };
-
-  render() {
-    return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div className="form-group">
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input form-control',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-    );
-  }
-}
-
+import LocationSearchInput from '../../../components/LocationSearchInput'
 
 function WhereAreYou() {
   const [location, setLocation] = useState(null)
