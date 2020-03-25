2d090ffeb6fce6bbad93c079dcc3ca5feb07e51f
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-25 01:10:19 -0400

    add `within` query scope to help_request

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,2 +1,5 @@
 class HelpRequest < ApplicationRecord
+  scope :within, -> (longitude, latitude, radius_km = 1) {
+    where("ST_Distance(address_lonlat, 'POINT(#{longitude} #{latitude})') < #{radius_km * 1000}")
+  }
 end
