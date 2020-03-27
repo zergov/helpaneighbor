f351b9a53de2694c3fe4ee082bed4133a0dba442
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-27 18:49:51 -0400

    ayoye beaucoup de changement juste pour une validation.......................

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def special_conditions
-    SpecialCondition.default_conditions
+  def error_message_for(model, field)
+    if model.errors.messages[field].any?
+      "<div class='input-error-message'>#{model.errors.messages[field].first}</div>".html_safe
+    else
+      ""
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#show', as: :homepage
 
-  get '/help-requests', to: 'help_requests#new'
-  get '/help-requests/:id', to: 'help_requests#show'
-  post '/help-requests', to: 'help_requests#create'
+  get '/help-requests', to: 'help_requests#new', as: :new_help_request
+  get '/help-requests/:id', to: 'help_requests#show', as: :help_request
+  post '/help-requests', to: 'help_requests#create', as: :help_requests
   get '/find-requests', to: 'help_requests#find'
 
   get '/help-now', to: 'help_now#show'
