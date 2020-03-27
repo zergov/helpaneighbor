2254736610a965c670a27505cad4f9f692538db7
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-27 06:13:44 -0400

    fetch help_requests in a 2km radius

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,5 +1,5 @@
 class HelpRequest < ApplicationRecord
-  scope :within, -> (longitude, latitude, radius_km = 1) {
+  scope :within, -> (longitude, latitude, radius_km = 2) {
     where("ST_Distance(address_lonlat, 'POINT(#{longitude} #{latitude})') < #{radius_km * 1000}")
   }
 end
