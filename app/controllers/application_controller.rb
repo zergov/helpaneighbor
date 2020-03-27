6fa56c73a11a03b1448dcb6111da3965c7902e67
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-27 19:20:20 -0400

    set visitor_token cookie on page load

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_visitor_cookie, unless: -> { cookies["visitor_token"] }
+
+  def set_visitor_cookie
+    cookies["visitor_token"] = SecureRandom.uuid
+  end
 end
--- test/controllers/help_requests_controller_test.rb
+++ test/controllers/help_requests_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class HelpRequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get help_requests_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get help_requests_create_url
-    assert_response :success
-  end
-
 end
