7645be4a675206ece65129da6f86c15195797fae
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-27 08:17:02 -0400

    add help_request#special_conditions

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -4,4 +4,10 @@ class HelpRequest < ApplicationRecord
   scope :within, -> (longitude, latitude, radius_km = 2) {
     where("ST_Distance(address_lonlat, 'POINT(#{longitude} #{latitude})') < #{radius_km * 1000}")
   }
+
+  def special_conditions
+    SpecialCondition.default_conditions.select do |condition|
+      conditions.include?(condition.name)
+    end
+  end
 end
