6fc04b2c5ef8fa44fe1c06b6921e7feebae19281
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-29 18:07:56 -0400

    check if database exists before launching container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 set -e
 
-bundle exec rails db:migrate
+bundle exec rails db:exists && bundle exec rails db:migrate || bundle exec rails db:setup
 
 rm -f tmp/pids/server.pid
 bundle exec rails s -b 0.0.0.0
