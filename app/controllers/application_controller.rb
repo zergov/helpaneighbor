cee1bf428ab581f70c9652b2195608b3e373d8a1
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-28 21:50:41 -0400

    remove visitor tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_visitor_cookie, unless: -> { cookies["visitor_token"] }
-
-  def set_visitor_cookie
-    cookies["visitor_token"] = SecureRandom.uuid
-  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,20 +1,4 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "visitor_token is set if not already set" do
-    refute cookies["visitor_token"]
-    get homepage_path
-    assert_response :success
-    assert @response.cookies["visitor_token"]
-  end
-
-  test "visitor_token is not overriden if its already set" do
-    get homepage_path
-    assert_response :success
-    visitor_token = @response.cookies["visitor_token"]
-
-    cookies["visitor_token"] = visitor_token
-    get homepage_path
-    assert_nil @response.cookies["visitor_token"]
-  end
 end
