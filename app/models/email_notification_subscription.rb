b63b57ffdfb604540ec56f17220f433192965544
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-31 17:03:58 -0400

    set coordinates before save on ens

--- app/models/email_notification_subscription.rb
+++ app/models/email_notification_subscription.rb
@@ -1,7 +1,15 @@
 class EmailNotificationSubscription < ApplicationRecord
+  before_save :set_coordinates
+
   after_create :send_confirmation_email
 
   def send_confirmation_email
     UserNotifierEmailMailer.send_subscription_email_confirmation(self).deliver
+  end
+
+  private
+
+  def set_coordinates
+    self.address_lonlat = "POINT(#{address_lon} #{address_lat})" if address_lon && address_lat
   end
 end
