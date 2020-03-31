8a46a4caa171d8d19dd0b5aec52d0c144dbebbee
Jonathan Lalande <lalandej.gg@gmail.com>
2020-03-31 18:24:03 -0400

    configure sendgrid mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  domain: 'helpaneighbor.ca',
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'apikey',
+  password:       ENV['SENDGRID_API_KEY']
+}
